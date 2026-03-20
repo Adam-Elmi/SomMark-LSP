0c62534f2bc2881ccebd4fc23f0f48b1f2b50a11
Adam-Elmi <Adamcade123@gmail.com>
2026-03-20 03:30:32 +0300

    Created File: add editors/vscode/extension.js

---
+++ editors/vscode/extension.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { LanguageClient, TransportKind } = require('vscode-languageclient/node');
+
+let client;
+
+function activate(context) {
+    // Path to the LSP server module (absolute path to the SomMark-LSP server/server.js)
+    const serverModule = path.join(__dirname, '..', '..', 'server', 'server.js');
+    console.info(`SomMark LSP: Starting server from ${serverModule}`);
+
+    const serverOptions = {
+        run: {
+            module: serverModule,
+            transport: TransportKind.stdio
+        },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.stdio
+        }
+    };
+
+    const clientOptions = {
+        // Register the server for .smark files
+        documentSelector: [{ scheme: 'file', language: 'sommark' }]
+    };
+
+    client = new LanguageClient(
+        'sommarkLSP',
+        'SomMark Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client (and the server)
+    client.start();
+    console.info('SomMark LSP: Client started.');
+}
+
+function deactivate() {
+    if (client) {
+        return client.stop();
+    }
+}
+
+module.exports = { activate, deactivate };
